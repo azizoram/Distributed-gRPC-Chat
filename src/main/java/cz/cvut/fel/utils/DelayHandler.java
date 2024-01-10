cb1b6b75f0cefe48498b628cb7de1cfb188c7caf
Ramir <azizoram@cvut.cz>
2024-01-10 19:32:34 +0100

    Implement delay handler, some refactoring

--- src/main/java/cz/cvut/fel/utils/DelayHandler.java
+++ src/main/java/cz/cvut/fel/utils/DelayHandler.java
@@ -1,6 +1,7 @@
 package cz.cvut.fel.utils;
 
 public interface DelayHandler {
+    void set(String param, String value);
     void handleRequestDelay(String method);
     void handleResponseDelay(String method);
 }
--- src/main/java/cz/cvut/fel/utils/NoDelayHandler.java
+++ src/main/java/cz/cvut/fel/utils/NoDelayHandler.java
@@ -2,6 +2,11 @@ package cz.cvut.fel.utils;
 
 public class NoDelayHandler implements DelayHandler{
     @Override
+    public void set(String param, String value) {
+
+    }
+
+    @Override
     public void handleRequestDelay(String method) {
 
     }
